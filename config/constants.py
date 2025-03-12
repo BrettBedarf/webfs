02b0b87fe1d82bfb9fed78fc2dad41814b7f778e
Brett Bedarf <brettbedarf.dev@gmail.com>
2025-03-11 22:22:46 -0600

    refactor: split into modules

---
+++ config/constants.py
@@ -0,0 +1,12 @@
+MAX_FH = (
+    2**31 - 1
+)  # 32-bits ensures compat with libfuse and more than enough open handles
+
+# Read cache configuration
+DEFAULT_CHUNK_SIZE = 2 * 1024 * 1024  # 4MB per chunk
+CACHE_MAX_SIZE = 200 * 1024 * 1024  # 100MB total
+NUM_CACHE_CHUNKS = CACHE_MAX_SIZE // DEFAULT_CHUNK_SIZE
+
+MAX_PREFETCH_AHEAD = 100 * 1024 * 1024  # e.g., 100MB ahead
+# How many chunks to fetch concurrently each batch.
+PREFETCH_BATCH_SIZE = 6
