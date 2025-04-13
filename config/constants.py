ed1315990c4085da743818b8e5ef1aebbb52d2b0
Brett Bedarf <brettbedarf.dev@gmail.com>
2025-04-13 16:31:47 -0600

    try lower chunk sizes

--- config/constants.py
+++ config/constants.py
@@ -3,10 +3,10 @@ MAX_FH = (
 )  # 32-bits ensures compat with libfuse and more than enough open handles
 
 # Read cache configuration
-DEFAULT_CHUNK_SIZE = 2 * 1024 * 1024  # 4MB per chunk
-CACHE_MAX_SIZE = 200 * 1024 * 1024  # 100MB total
+DEFAULT_CHUNK_SIZE = 1 * 1024 * 1024  # 1MB per chunk
+CACHE_MAX_SIZE = 200 * 1024 * 1024  # 200MB total
 NUM_CACHE_CHUNKS = CACHE_MAX_SIZE // DEFAULT_CHUNK_SIZE
 
 MAX_PREFETCH_AHEAD = 100 * 1024 * 1024  # e.g., 100MB ahead
 # How many chunks to fetch concurrently each batch.
-PREFETCH_BATCH_SIZE = 6
+PREFETCH_BATCH_SIZE = 3
